f862284772909bc7ea472d80c2a14d9a981dbfe3
zhanggaofeng <zhanggaofeng@kuaishou.com>
2023-07-05 10:48:41 +0800

    add guava

---
+++ guava-learning/src/main/java/com/peakzz/Main.java
@@ -0,0 +1,11 @@
+package com.peakzz;
+
+/**
+ * @author yourname <zhanggaofeng@kuaishou.com>
+ * Created on 2023-07-05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ guava-learning/src/main/java/guava/String_module.java
@@ -0,0 +1,9 @@
+package guava;
+
+/**
+ * @author yourname <zhanggaofeng@kuaishou.com>
+ * Created on 2023-07-05
+ */
+public class String_module {
+
+}
