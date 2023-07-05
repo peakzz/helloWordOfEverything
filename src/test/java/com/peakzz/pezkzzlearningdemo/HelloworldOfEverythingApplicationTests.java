3337499af788b5848bd43d1ebb212ca21042176d
zhanggaofeng <zhanggaofeng@kuaishou.com>
2023-07-05 10:12:11 +0800

    first commit

---
+++ src/test/java/com/peakzz/pezkzzlearningdemo/HelloworldOfEverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peakzz.pezkzzlearningdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldOfEverythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
